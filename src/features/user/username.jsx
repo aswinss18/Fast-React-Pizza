a10aabca38abe87a430a5902fe1d959002593f56
aswinss18 <aswinartography18@gmail.com>
2024-09-20 19:57:48 +0530

    basic box model,text styling and responsive designing is created using tailwind classes

---
+++ src/features/user/username.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function username() {
+  return <div className="text-sm font-semibold hidden">Aswin</div>;
+}
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
-
+import Username from "../features/user/username";
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 px-4 py-3 uppercase border-b border-stone-200 sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
+      <Username />
     </header>
   );
 }
