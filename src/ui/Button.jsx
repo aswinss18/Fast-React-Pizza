bea44bb01950d9906a7b8fa9d9c60c1489cb2570
aswinss18 <aswinartography18@gmail.com>
2024-09-22 12:36:12 +0530

    created some reusable components

---
+++ src/ui/Button.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Button({ children, disabled, to }) {
+  const className =
+    "inline-block bg-yellow-400 px-4 py-3 font-semibold uppercase text-stone-800 tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-4";
+
+  if (to) {
+    <Link to={to}>{children}</Link>;
+  }
+
+  return (
+    <button disabled={disabled} className={className}>
+      {children}
+    </button>
+  );
+}
