0e877b9459978b86a8f59df2aede6e4c6e0103be
aswinss18 <aswinartography18@gmail.com>
2024-09-22 10:33:12 +0530

    Focus ring,transition and basic styling are given to the Order Now button

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -78,7 +78,10 @@ function CreateOrder() {
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
 
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="inline-block bg-yellow-400 px-4 py-3 font-semibold uppercase text-stone-800 tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2"
+          >
             {isSubmitting ? "Placing order..." : "Order now"}
           </button>
         </div>
