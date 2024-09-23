9fc3fcaf03c7af6730ebb9222d385d46e18c5e54
aswinss18 <aswinartography18@gmail.com>
2024-09-23 07:03:57 +0530

    Menu is styled

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Button({ children, disabled, to }) {
-  const className =
-    "inline-block bg-yellow-400 px-4 py-3 font-semibold uppercase text-stone-800 tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-4";
+export default function Button({ children, disabled, to, type }) {
+  const base =
+    "inline-block bg-yellow-400  font-semibold uppercase text-stone-800 tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+  const styles = {
+    primary: base + " px-4 py-3 md:px-6 md:py-4 ",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+  };
 
   if (to) {
     return (
-      <Link className={className} to={to}>
+      <Link className={styles[type]} to={to}>
         {children}
       </Link>
     );
   }
 
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
